6c301575e6d022f90539b639d5bcb20101f4791c
JoaoCampos767 <joaopedro767767@gmail.com>
2023-01-28 09:38:34 -0300

    ajuste

--- lib/helpers/loginHelper.dart
+++ lib/helpers/loginHelper.dart
@@ -2,6 +2,13 @@ import 'package:sla/model/loginModel.dart';
 
 class LoginHelper {
   String validaLogin(LoginModel login) {
-    return "";
+    var _msg;
+
+    if (login.email.isNotEmpty) {
+    } else {
+      _msg = "E-mail é obrigatório, por favor preencha o mesmo!";
+    }
+
+    return _msg;
   }
 }
--- lib/telas/login.dart
+++ lib/telas/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sla/helpers/loginHelper.dart';
 import 'package:sla/telas/cadastro.dart';
 
 class Login extends StatefulWidget {
@@ -9,6 +10,32 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final TextEditingController _controllerEmail = TextEditingController();
+  final TextEditingController _controllerSenha = TextEditingController();
+
+  String _mensagemErro = "";
+
+  _validarCampos() {
+    String email = _controllerEmail.text;
+    String senha = _controllerSenha.text;
+
+    if (email.isNotEmpty && email.contains("@")) {
+      if (senha.isNotEmpty) {
+        setState(() {
+          _mensagemErro = "";
+        });
+      } else {
+        setState(() {
+          _mensagemErro = "Preencha a senha!";
+        });
+      }
+    } else {
+      setState(() {
+        _mensagemErro = "Preencha o E-mail utilizando @";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +52,7 @@ class _LoginState extends State<Login> {
                 Padding(
                   padding: const EdgeInsets.only(bottom: 10),
                   child: TextField(
+                    controller: _controllerEmail,
                     keyboardType: TextInputType.emailAddress,
                     style: const TextStyle(fontSize: 20),
                     decoration: InputDecoration(
@@ -39,6 +67,7 @@ class _LoginState extends State<Login> {
                 ),
                 TextField(
                   obscureText: true,
+                  controller: _controllerSenha,
                   keyboardType: TextInputType.text,
                   style: const TextStyle(fontSize: 20),
                   decoration: InputDecoration(
@@ -54,7 +83,9 @@ class _LoginState extends State<Login> {
                 Padding(
                   padding: const EdgeInsets.only(top: 16, bottom: 10),
                   child: ElevatedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      _validarCampos();
+                    },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: const Color.fromARGB(255, 167, 92, 180),
                       padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
@@ -86,7 +117,16 @@ class _LoginState extends State<Login> {
                       style: TextStyle(color: Colors.black),
                     ),
                   ),
-                )
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Center(
+                    child: Text(
+                      _mensagemErro,
+                      style: const TextStyle(color: Colors.red, fontSize: 20),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
